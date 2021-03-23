716116acaf09d69be2ec63078977bc648c624fd4
Woohyuk Jang <spectat@kookmin.ac.kr>
2021-03-23 11:47:49 +0900

    Update is_encrypt.py

--- preprocessing/is_encrypt.py
+++ preprocessing/is_encrypt.py
@@ -1,45 +1,29 @@
 from tqdm import tqdm
 from collections import Counter
-import math
+
 import numpy as np
-import secrets
-import payload_parser
-import os
 import pandas as pd
+import math
+import secrets
+import os
+
+import payload_parser
+
 LOOP = 10000
 threshold = 6
 
-
-class Is_encrypt():
+class IsEncrypt():
     def __init__(self, path):
-        if "std_dict.pickle" not in os.listdir(r"."):
+        if f"std_dict{os.extsep}pickle" not in os.listdir(os.curdir):
             self.std(LOOP)
         self.stat = pd.read_pickle("std_dict.pickle")
 
-        dir_save_path = {"plain": rf"{path}\plain",
-                         "encrypt": rf"{path}\encrypt"}
+        dir_save_path = {"plain": rf"{path}{os.sep}plain",
+                         "encrypt": rf"{path}{os.sep}encrypt"}
 
         for key in dir_save_path.keys():
             if not os.path.exists(dir_save_path[key]):
                 os.makedirs(dir_save_path[key])
-
-    def is_encrypt(self, ful_payload):
-        exc = ("170303", "170302", "170301", "150301", "150302", "150303")
-        wl = ("140301", "140302", "140303", "160301", "160302", "160303")
-
-        payload = self.get_app(ful_payload)
-        length = int(len(payload)/2)
-        if length <= 3:
-            return False
-        elif payload[:6] in exc:
-            return True
-        elif payload[:6] in wl:
-            return False
-        elif (self.stat['MEAN'][length] -
-              (threshold * self.stat['STD'][length]) < self.get_entropy(payload_parser.to_byte(payload))):
-            return True
-        else:
-            return False
 
     def get_entropy(self, data):
         if not data:
@@ -69,3 +53,21 @@ class Is_encrypt():
             stat['MEAN'].append(sum(entropy_list) / LOOP)
             stat['STD'].append(np.std(entropy_list))
         pd.to_pickle(stat, "std_dict.pickle")
+
+    def encryption_determinate(self, ful_payload):
+        bl = ("170303", "170302", "170301", "150301", "150302", "150303")
+        wl = ("140301", "140302", "140303", "160301", "160302", "160303")
+
+        payload = self.get_app(ful_payload)
+        length = int(len(payload)/2)
+        if length <= 3:
+            return False
+        elif payload[:6] in bl:
+            return True
+        elif payload[:6] in wl:
+            return False
+        elif (self.stat['MEAN'][length] -
+              (threshold * self.stat['STD'][length]) < self.get_entropy(payload_parser.to_byte(payload))):
+            return True
+        else:
+            return False
